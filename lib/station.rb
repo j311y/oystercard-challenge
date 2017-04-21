ef9eda6ca50e5605a47396aad0081009301cfa35
Jonathan Elliot <jonathan.j.elliot@gmail.com>
2017-04-21 15:48:54 +0100

    adds 1  argument to #zone

--- lib/station.rb
+++ lib/station.rb
@@ -3,7 +3,7 @@ class Station
 
   @stations = {:Waterloo => 1}
 
-  def zone
+  def zone(station_name)
   end
 
 end
--- spec/station_spec.rb
+++ spec/station_spec.rb
@@ -1,12 +1,16 @@
 require 'station'
 
 describe Station do
-	# subject(:station) { described_class.new }
+	subject(:station) { described_class.new }
 
 	it { is_expected.to respond_to(:zone).with(1).argument}
 
-  # describe '#zone' do
-  #   it ''
-  # end
+  describe '#zone' do
+		let(:station_name){ double :station_name }
+    it 'raises an error if station does not exist' do
+    	station.zone(:station_name)
+			expect { :station_name != :stations }.to raise_error 'Station does not exist'
+    end
+  end
 
 end
