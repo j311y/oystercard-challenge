83f4f5b4c04657839e0037c4c4c26990cfcc3a79
Jonathan Elliot <jonathan.j.elliot@gmail.com>
2017-04-21 14:25:29 +0100

    added #stations hash and test

--- spec/station_spec.rb
+++ spec/station_spec.rb
@@ -1,11 +1,18 @@
 require 'station'
 
 describe Station do
+  subject { described_class.new(station_name: "Waterloo", zone: 1) }
 
-	describe '#new' do
-		it 'takes 2 arguments' do
-			expect(Station).to respond_to(:initialize).with(2).arguments
+	# describe '#new' do
+	# 	it 'takes 2 arguments' do
+	# 		expect(Station).to respond_to(:new).with(2).arguments
+	# 	end
+
+	describe '#station_name' do
+		it 'returns the #zone' do
+			subject.station_name
+      expect(subject.zone).to_not be_nil
 		end
 	end
+ # end
 end
-
