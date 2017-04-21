063c5cee2420f7a89511a2c66c6cbf8b48297a91
BenNoonan1991 <ben.noonan2@gmail.com>
2017-04-21 15:37:51 +0100

    Adds test for number of arguments for zone method

--- lib/station.rb
+++ lib/station.rb
@@ -3,4 +3,7 @@ class Station
 
   @stations = {:Waterloo => 1}
 
+  def zone
+  end
+
 end
--- spec/station_spec.rb
+++ spec/station_spec.rb
@@ -3,6 +3,10 @@ require 'station'
 describe Station do
 	# subject(:station) { described_class.new }
 
-	it { is_expected.to respond_to :zone }
+	it { is_expected.to respond_to(:zone).with(1).argument}
+
+  # describe '#zone' do
+  #   it ''
+  # end
 
 end
